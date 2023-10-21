fe15b3c331a5a556fafcc59674f0d572a8538234
hiro502 <hiro1887v@gmail.com>
2023-10-21 23:31:13 +0200

    achevement du carrousel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,42 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const slidImage = document.getElementById("slideImage");
+const slideText = document.getElementById("slideText");
+const slideDot = document.getElementById("slideDot")
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+let i = 0;
+
+right.addEventListener("click", function(){
+	if(i + 1 < slides.length){
+		i++;
+		slideImage.src = (`./assets/images/slideshow/${slides[i].image}`);
+		slideText.innerHTML = slides[i].tagLine;
+		slideDot.children[i].className ="dot dot_selected";
+		slideDot.children[i - 1].className ="dot";
+	}else{ 
+		i -= 3;
+		slideImage.src = (`./assets/images/slideshow/${slides[i].image}`);
+		slideText.innerHTML = slides[i].tagLine;
+		slideDot.children[i].className ="dot dot_selected";
+		slideDot.children[3].className ="dot";
+	 }
+});
+
+left.addEventListener("click", function(){
+	if(i > 0){
+		i--;
+		slideImage.src = (`./assets/images/slideshow/${slides[i].image}`);
+		slideText.innerHTML = slides[i].tagLine;
+		slideDot.children[i].className ="dot dot_selected";
+		slideDot.children[i + 1].className ="dot";
+	}else{
+		i += 3;
+		slideImage.src = (`./assets/images/slideshow/${slides[i].image}`);
+		slideText.innerHTML = slides[i].tagLine;
+		slideDot.children[i].className ="dot dot_selected";
+		slideDot.children[0].className ="dot";
+	}
+});
